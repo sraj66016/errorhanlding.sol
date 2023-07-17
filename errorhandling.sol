84bf827d261a7e9dc701bb82c2449574ec28c6f4
sraj66016 <100196269+sraj66016@users.noreply.github.com>
2023-07-17 13:18:59 +0530

    Update errorhandling.sol

--- errorhandling.sol
+++ errorhandling.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ErrorHandlingExample {
-    function checkValue(uint _value) public pure returns (string memory) {
+contract solidityErrorHandling {
+    function checkValue(uint _value) public returns (string memory) {
         require(_value > 0, "Value must be greater than zero");
 
-        if (_value % 2 == 0) {
+        if (value % 2 == 0) {
             return "Value is even";
         } else {
-            assert(_value != 5);
+            assert(value != 5);
             return "Value is odd";
         }
     }
 
-    function sendEther(address payable _recipient) public payable {
+    function sendmessagevalue(address payable _recipient) public payable {
         require(msg.value > 0, "Sent value must be greater than zero");
 
         if (msg.value >= 1 ether) {
